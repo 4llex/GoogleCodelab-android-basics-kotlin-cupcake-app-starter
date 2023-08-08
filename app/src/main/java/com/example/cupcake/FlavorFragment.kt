2464978da9003a6c32ab11474ab819dfad6d8a88
Alex Rosa <alexr@gec.inatel.br>
2023-08-07 21:58:47 -0300

    Complete the navgation graph

--- app/src/main/java/com/example/cupcake/FlavorFragment.kt
+++ app/src/main/java/com/example/cupcake/FlavorFragment.kt
@@ -21,6 +21,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentFlavorBinding
 
 /**
@@ -46,7 +47,9 @@ class FlavorFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding?.apply {
-            nextButton.setOnClickListener { goToNextScreen() }
+            nextButton.setOnClickListener {
+                goToNextScreen()
+            }
         }
     }
 
@@ -54,7 +57,7 @@ class FlavorFragment : Fragment() {
      * Navigate to the next screen to choose pickup date.
      */
     fun goToNextScreen() {
-        Toast.makeText(activity, "Next", Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_flavorFragment_to_pickupFragment)
     }
 
     /**
